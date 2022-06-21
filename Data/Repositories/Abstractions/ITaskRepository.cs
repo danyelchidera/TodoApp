279322663801ffe375d186848f7cbd433af7cba6
danyelchidera <danyelchidera1@gmail.com>
2022-06-21 11:25:05 +0100

    Add project files.

---
+++ Data/Repositories/Abstractions/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories.Abstractions
+{
+    public interface ITaskRepository
+    {
+        Task CreateTask( Task task );
+    }
+}
